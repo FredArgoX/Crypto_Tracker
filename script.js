2a95a046afe6b7201776b0095d5602e3157d1808
FredArgoX <fredy.argo.3@gmail.com>
2025-12-22 02:16:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const assets = [
     { name: "HEX (ETH)", amount: 537_000, url: "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x9e0905249CeEFfFB9605E034b534544684A58BE6" },
     //{ name: "eHEX (PLS)", amount: 72_980, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xF0eA3efE42C11c8819948Ec2D3179F4084863D3F" },
-    { name: "HEX", amount: 820_000, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf1F4ee610b2bAbB05C635F726eF8B0C568c8dc65" },
+    { name: "HEX", amount: 870_000, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf1F4ee610b2bAbB05C635F726eF8B0C568c8dc65" },
     { name: "PLS", amount: 50_000, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xE56043671df55dE5CDf8459710433C10324DE0aE" },
     { name: "PLSX", amount: 460_397_124, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0x1b45b9148791d3a104184Cd5DFE5CE57193a3ee9" },
     { name: "INC", amount: 631, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf808Bb6265e9Ca27002c0A04562Bf50d4FE37EAA" },
