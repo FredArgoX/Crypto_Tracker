d8bab2082daa005f7dd179a74e864534374eb216
FredArgoX <fredy.argo.3@gmail.com>
2026-02-06 01:13:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const assets = [
     { name: "HEX (ETH)", amount: 500_000, url: "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x9e0905249CeEFfFB9605E034b534544684A58BE6" },
     //{ name: "eHEX (PLS)", amount: 72_980, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xF0eA3efE42C11c8819948Ec2D3179F4084863D3F" },
-    { name: "HEX", amount: 1_943_366, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf1F4ee610b2bAbB05C635F726eF8B0C568c8dc65" },
+    { name: "HEX", amount: 2_000_513, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf1F4ee610b2bAbB05C635F726eF8B0C568c8dc65" },
     { name: "PLS", amount: 50_000, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xE56043671df55dE5CDf8459710433C10324DE0aE" },
-    { name: "PLSX", amount: 511_940_000, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0x1b45b9148791d3a104184Cd5DFE5CE57193a3ee9" },
+    { name: "PLSX", amount: 500_209_244, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0x1b45b9148791d3a104184Cd5DFE5CE57193a3ee9" },
     { name: "INC", amount: 631, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0xf808Bb6265e9Ca27002c0A04562Bf50d4FE37EAA" },
     { name: "XEN (ETH)", amount: 30_808_114_900, url: "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x2a9d2ba41aba912316D16742f259412B681898Db" },
     //{ name: "pXEN", amount: 1_411_058_764_239, url: "https://api.dexscreener.com/latest/dex/pairs/pulsechain/0x61C8D2DeE20F8e303B999D485cFa577054196B40" },
